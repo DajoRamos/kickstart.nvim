7e433aaa02690811272c500a1f839adac8c406b3
David Ramos <david.ramos@proanco.net>
2025-03-24 17:43:19 -0500

    Add terraform lsp and formatter

--- init.lua
+++ init.lua
@@ -681,7 +681,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+        graphql = {},
+        terraformls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -690,7 +692,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -777,13 +779,15 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        php = { 'php_cs_fixer' },
+        php = { 'pretty-php' },
         -- Conform can also run multiple formatters sequentially
         python = { 'black' },
+        terraform = { 'terraform_fmt', 'tflint' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier', stop_after_first = true } },
+        graphql = { { 'prettierd', 'prettier', stop_after_first = true } },
         typescript = { { 'prettierd', 'prettier', stop_after_first = true } },
         typescriptreact = { { 'prettierd', 'prettier', stop_after_first = true } },
       },
@@ -810,12 +814,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
